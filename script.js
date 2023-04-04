a13e429589c362c058937afa138555f910a85fc1
flowtyf <taiao.kawiti@developersinstitute.co.nz>
2023-04-04 15:01:48 +1200

    Add comments

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Main container for etchasketch
 const gridContainer = document.createElement('div');
 gridContainer.className = 'grid-container';
 document.body.appendChild(gridContainer);
 
+// Set up the amount of squares for the etchasktech
 const renderGrid = (squares) => {
   for (let i = 0; i < squares; i++) {
     const gridSquare = document.createElement('div');
@@ -10,16 +12,18 @@ const renderGrid = (squares) => {
   }
 };
 
-const drawPixel = () => {
-  const getSquares = document.getElementsByClassName('grid-square');
-  const squareArr = Array.from(getSquares);
-  squareArr.forEach((el) => {
-    el.addEventListener('mouseover', () => {
-      el.classList.add('black-bg');
+//Add an event listener that listens for mouseover, change color of grid square to black
+function drawPixel() {
+    const getSquares = document.getElementsByClassName('grid-square');
+    const squareArr = Array.from(getSquares);
+    squareArr.forEach((el) => {
+        el.addEventListener('mouseover', () => {
+            el.classList.add('black-bg');
+        });
     });
-  });
-};
+}
 
+// init button for resizing
 const gridSizeButton = document.createElement('button');
 gridSizeButton.className = 'size-button';
 gridSizeButton.innerHTML = '64x64';
